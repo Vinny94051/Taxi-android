2a9e5848559c1cfa8b142858d6bfc46ff7467e12
Vinny94051 <winny455@gmail.com>
2020-01-24 18:40:31 +0300

    change margin

--- app/src/main/java/com/example/taximuslim/data/network/api/AuthApi.kt
+++ app/src/main/java/com/example/taximuslim/data/network/api/AuthApi.kt
@@ -2,14 +2,12 @@ package com.example.taximuslim.data.network.api
 
 import com.example.taximuslim.data.network.dto.NumberRequest
 import com.example.taximuslim.data.network.dto.auth.CheckSmsCodeRequest
-import com.example.taximuslim.data.network.dto.auth.CheckSmsCodeResponse
 import com.example.taximuslim.data.network.dto.auth.NumberRegistrationStatusResponse
 import com.example.taximuslim.data.network.dto.auth.PreseptResponse
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
-import javax.inject.Inject
 
 interface AuthApi {
 
